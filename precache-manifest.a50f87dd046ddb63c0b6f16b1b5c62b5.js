8fa8fd14476b502e0c6faf1851bfb2347aa1783c
Deepanshu shekhar <dshekhar950@gmail.com>
2024-07-29 16:04:43 +0530

    Updates

---
+++ precache-manifest.a50f87dd046ddb63c0b6f16b1b5c62b5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "442622302d469d9aa6660ce9a0c7dca2",
+    "url": "/Book-Library/index.html"
+  },
+  {
+    "revision": "ca4d73ae304d1b914425",
+    "url": "/Book-Library/static/css/main.5803de69.chunk.css"
+  },
+  {
+    "revision": "2429766700004c8b8e34",
+    "url": "/Book-Library/static/js/2.caa3e875.chunk.js"
+  },
+  {
+    "revision": "ca4d73ae304d1b914425",
+    "url": "/Book-Library/static/js/main.d8e49d52.chunk.js"
+  },
+  {
+    "revision": "4d4ebead368d0f804c03",
+    "url": "/Book-Library/static/js/runtime~main.fe3d631d.js"
+  },
+  {
+    "revision": "644fc12a37037247c501e03bb06e09e7",
+    "url": "/Book-Library/static/media/about-img.644fc12a.jpg"
+  },
+  {
+    "revision": "9c4bbb21ac32475a2f3d8c55d2b7337d",
+    "url": "/Book-Library/static/media/cover_not_found.9c4bbb21.jpg"
+  },
+  {
+    "revision": "854b7295d17d52ac8b25aa2911cbf2a2",
+    "url": "/Book-Library/static/media/library-img.854b7295.jpg"
+  },
+  {
+    "revision": "07af62b9d62694f6d6e5340279ac5ab7",
+    "url": "/Book-Library/static/media/loader.07af62b9.svg"
+  }
+]);
